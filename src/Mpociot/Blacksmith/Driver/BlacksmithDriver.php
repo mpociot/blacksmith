6b78b9eedb674d78b3399c0bd2be51093c24b67e
Alan Wynn <alan.wynn@me.com>
2016-11-17 15:04:35 +0000

    update the Driver to have PUT
    Signed-off-by: Alan Wynn <alan.wynn@me.com>

--- src/Mpociot/Blacksmith/Driver/BlacksmithDriver.php
+++ src/Mpociot/Blacksmith/Driver/BlacksmithDriver.php
@@ -17,4 +17,16 @@ class BlacksmithDriver extends GoutteDriver
             'HTTP_X_XSRF_TOKEN' => $this->getClient()->getCookieJar()->get('XSRF-TOKEN')->getValue()
         ], $postData);
     }
+
+    /**
+     * Perform a PUT request
+     */
+    public function put($url, $postData)
+    {
+        $this->getClient()->request('PUT', $this->prepareUrl($url), [], [], [
+            'HTTP_ACCEPT' => 'application/json',
+            'HTTP_CONTENT_TYPE' => 'application/json',
+            'HTTP_X_XSRF_TOKEN' => $this->getClient()->getCookieJar()->get('XSRF-TOKEN')->getValue()
+        ], $postData);
+    }
 }
