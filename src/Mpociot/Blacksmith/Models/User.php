2c80c6a54b2bd0033c1a462a3d2119984504f9d4
Peter Steenbergen <psteenbergen@gmail.com>
2016-11-18 16:28:20 +0100

    Removed obsolete array

--- src/Mpociot/Blacksmith/Models/User.php
+++ src/Mpociot/Blacksmith/Models/User.php
@@ -4,11 +4,4 @@ namespace Mpociot\Blacksmith\Models;
 
 class User extends ForgeModel
 {
-    /**
-     * @return array
-     */
-    public function toArray()
-    {
-        return $this->data->toArray();
-    }
 }
