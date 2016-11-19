326ba51282df239e1f61dd6bf69d17a2cf3f6bf9
Peter Steenbergen <psteenbergen@gmail.com>
2016-11-19 14:18:21 +0100

    Update circle members

--- src/Mpociot/Blacksmith/Models/Circle.php
+++ src/Mpociot/Blacksmith/Models/Circle.php
@@ -8,15 +8,37 @@ class Circle extends ForgeModel
 {
     protected $id;
 
+    /**
+     * Invite a new forge user to a Circle
+     *
+     * @param $email
+     * @return Circle
+     * @throws Exception
+     */
     public function inviteMember($email)
     {
-        $result = $this->browser->postContent('https://forge.laravel.com/circles/'.$this->id.'/invite/', [
+        $result = $this->browser->postContent('https://forge.laravel.com/circles/'.$this->id.'/invite', [
             'email' => $email,
         ]);
 
         if ($this->browser->getSession()->getStatusCode() === 422) {
             throw new Exception('Error: '.print_r($result, true));
         }
+
+        return new Circle($result, $this->browser);
+    }
+
+    /**
+     * Update all members of the circle
+     *
+     * @param array $member_ids
+     * @return Circle
+     */
+    public function updateMembers($member_ids)
+    {
+        $result = $this->browser->putContent('https://forge.laravel.com/circles/'.$this->id.'/members', [
+            'members' => $member_ids,
+        ]);
 
         return new Circle($result, $this->browser);
     }
